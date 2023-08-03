e73684f4e136070bb38aab7a3b2424dea251acf7
Mausam Sharma <mausam22sharma@gmail.com>
2023-08-03 11:59:18 +0530

    first commit

--- setup.py
+++ setup.py
@@ -3,24 +3,24 @@ from typing import List
 
 HYPEN_E_DOT='-e .'
 
-def get_requirements(file_path:str)->list[str]:
+def get_requirements(file_path:str)->List[str]:
     requirements=[]
-    with open (file_path)as file_obj:
+    with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
+        requirements=[req.replace("","") for req in requirements]
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
-
-            
         return requirements
 
 
 
-
 setup(
-    name='DiamondPricePrediction',
-    author_email='mausam22sharma@gmail.com',
-    install_requires=['pandas','sklearn'],
+    name='DiamondPriceprediction',
+    version='0.0.1',
+    author='Mausam Sharma',
+    author_email="mausam22sharma@gmail.com",
+    install_requires=get_requirements('requirements.txt'),
     packages=find_packages()
+
 )
